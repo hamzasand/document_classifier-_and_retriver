bef64051d86f3cf96e8fbad73bd2264f2f50e88b
hamzasand <mhamzasaeed762@gmail.com>
2026-05-24 21:19:46 +0500

    update all

--- document_classifier-_and_retriver/src/retrieval.py
+++ document_classifier-_and_retriver/src/retrieval.py
@@ -5,31 +5,20 @@ import numpy as np
 
 model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
 
-
 def build_index(documents):
-
     texts = [doc["text"] for doc in documents]
-
     embeddings = model.encode(texts)
-
     dimension = embeddings.shape[1]
-
     index = faiss.IndexFlatL2(dimension)
-
     index.add(np.array(embeddings))
-
     return index, embeddings
 
 
 def search(query, index, documents):
-
     query_embedding = model.encode([query])
-
     distances, indices = index.search(np.array(query_embedding), k=1)
-
     results = []
-
+    
     for idx in indices[0]:
         results.append(documents[idx]["filename"])
-
     return results
